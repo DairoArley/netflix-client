3d3d40e079503ee0166fcd2c5669e4655f00b724
DairoArley <dagavergara@gmail.com>
2023-05-25 22:32:28 -0500

    Se crea navbar grafico

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import { ArrowDropDown, Notifications, Search } from '@material-ui/icons'
 import './navbar.scss'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [IsScrolled, setIsScrolled] = useState(false);
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true)
+    return ()=> window.onscroll = null
+  }
   return (
-    <div className='navbar'>
+    <div className={IsScrolled ? "navbar scrolled" : "navbar"}>
         <div className="container">
             <div className="left">
                 <img src= "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="netflix"/>
@@ -15,11 +21,17 @@ const Navbar = () => {
                 <span>Explora por idiomas</span>
             </div>
             <div className="right">
-                <Search/>
+                <Search className='icon'/>
                 <span>Niños</span>
-                <Notifications/>
+                <Notifications className='icon'/>
                 <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="log"/>
-                <ArrowDropDown/>
+                <div className='profile'>
+                  <ArrowDropDown className='icon'/>
+                  <div className='options'>
+                    <span>Settings</span>
+                    <span>Logout</span>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,7 +4,80 @@ import './home.scss'
 const Home = () => {
   return (
     <div className='home'>        
-        <Navbar/>    
+        <Navbar/>
+        <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="log"/>    
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        v
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        <var>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+
+        v
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aperiam consequuntur, quas adipisci deleniti obcaecati ea! Praesentium, soluta? Quia quas accusantium quis placeat voluptatum. Esse voluptatem ullam at totam mollitia.
+        
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+
+        <var>v
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta, accusantium. Debitis unde vel laudantium odio asperiores, quam voluptatum deleniti, nesciunt voluptatibus possimus, consectetur odit ex quia doloremque eligendi atque fugit!
+        </var>
+
+        
+        </var>
     </div>
   )
 }
